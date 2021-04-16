44f99286fb5f77c54b2e92559eb37c5a7577e8e8
Jovan Stepanovic <jsjovan94@gmail.com>
2021-04-16 13:03:20 +0200

    Setting dependencies for Selenium, jUnit5, WebDriverManager

--- src/main/java/com/alasdoo/developercourseassignment/DeveloperCourseAssignment.java
+++ src/main/java/com/alasdoo/developercourseassignment/DeveloperCourseAssignment.java
@@ -1,5 +1,11 @@
 package com.alasdoo.developercourseassignment;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,11 +19,29 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class DeveloperCourseAssignment {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeveloperCourseAssignment.class, args);
-	}
+    public static void main(String[] args) {
 
-	/*@Bean
+        SpringApplication.run(DeveloperCourseAssignment.class, args);
+
+        /*
+        String projectPath = System.getProperty("user.dir");
+        System.setProperty("webdriver.gecko.driver", projectPath + "\\drivers\\geckodriver\\geckodriver.exe");
+         */
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        
+
+        driver.get("http://localhost:3000");
+        
+       WebElement Settings =  driver.findElement(By.linkText("Settings"));
+       Settings.click();
+       
+       WebElement StartButton = driver.findElement(By.tagName("button"));
+       StartButton.click();
+
+    }
+
+    /*@Bean
 	public WebMvcConfigurer corsConfigurer() {
 			return new WebMvcConfigurerAdapter() {
 					@Override
